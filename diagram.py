acc5c40c47b0b9fa00faa33a726ac3c70ef25fa3
Daranee Samanmit <93210361+daraneeS@users.noreply.github.com>
2022-10-21 10:11:45 -0700

    Update diagram.py

--- diagram.py
+++ diagram.py
@@ -4,8 +4,7 @@ from diagrams import Cluster, Diagram
 from diagrams.aws.storage import S3
 from diagrams.aws.compute import Lambda
 from diagrams.aws.integration import Eventbridge
-from diagrams.aws.management import Cloudwatch
-from diagrams.aws.management import CloudwatchEventTimeBased
+
 
 with Diagram("Work Flows", show=True):
     with Cluster("AWS"):
